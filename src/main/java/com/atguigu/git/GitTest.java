70779029bf3bfc02dbcf9dd63cc5307e14970a9b
zhy <zhy@atguigu.com>
2023-03-24 20:42:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 86130
+ * @create 2023-03-24 20:37
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
